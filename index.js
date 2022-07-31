601674f17f6f444964ddc582832ee6337ed15106
Danya <kloveazaks@gmail.com>
2022-07-31 16:32:41 +0300

    add mongoose and model User

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import dotenv from "dotenv";
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb+srv://kloveaz:1251@cluster0.a4ddwb9.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log(`[+] DB OK`))
+  .catch((err) => console.log(`[-] DB ERROR: ${err}`));
 
 const app = express();
 const PORT = process.env.PORT || 5100;
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Express</h1>");
+  res.redirect('/auth/register')
 });
 
-
+app.post("/auth/register", (req, res) => {
+  
+});
 
 app.listen(PORT, (err) => {
   if (err) {
